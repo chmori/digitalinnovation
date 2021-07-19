adef1d1a4d708349f0b592b9bd139a2531e71556
chmori <christopher_mori@hotmail.com>
2021-07-19 19:49:47 -0300

    updated manager class to testing interface

--- src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Gerente.kt
@@ -1,5 +1,12 @@
 package one.digitalinnovation.digionebank
 
-class Gerente(nome: String, cpf: String, salario: Double) : Funcionario(nome = nome, cpf = cpf, salario = salario) {
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double,
+    val senha: String
+) : Funcionario(nome = nome, cpf = cpf, salario = salario), Logavel {
     override fun calculoAuxilio(): Double = salario * 0.4
+
+    override fun login(): Boolean = "senha123" == senha
 }
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -1,11 +1,9 @@
 package one.digitalinnovation.digionebank.testes
-import one.digitalinnovation.digionebank.Analista
-import one.digitalinnovation.digionebank.Funcionario
-import one.digitalinnovation.digionebank.Gerente
-import one.digitalinnovation.digionebank.Pessoa
+import one.digitalinnovation.digionebank.*
 
 fun main() {
 
-    val maria = Gerente("Maria do Carmo", "1234567899",5000.0)
+    val maria = Gerente("Maria do Carmo", "1234567899",5000.0,"senha1235")
     ImprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autentica(maria)
 }
